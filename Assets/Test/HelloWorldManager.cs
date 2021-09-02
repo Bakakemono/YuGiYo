26b6e30f5e0a02558aa2724282281ecabf15503d
Bakakemono <robinalves1224@gmail.com>
2021-09-02 16:19:39 +0200

    Networking test now work

--- Assets/Test/HelloWorldManager.cs
+++ Assets/Test/HelloWorldManager.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 namespace HelloWorld {
     public class HelloWorldManager : MonoBehaviour {
 
-        private void OnGUI() {
+        void OnGUI() {
             GUILayout.BeginArea(new Rect(10, 10, 300, 300));
-            if(NetworkManager.Singleton.IsClient && !NetworkManager.Singleton.IsServer) {
+            if (!NetworkManager.Singleton.IsClient && !NetworkManager.Singleton.IsServer) {
                 StartButtons();
             }
             else {
@@ -19,31 +19,28 @@ namespace HelloWorld {
         }
 
         static void StartButtons() {
-            if (GUILayout.Button("Host"))
-                NetworkManager.Singleton.StartHost();
-
-            if (GUILayout.Button("Client"))
-                NetworkManager.Singleton.StartClient();
-
-            if (GUILayout.Button("Server"))
-                NetworkManager.Singleton.StartServer();
+            if (GUILayout.Button("Host")) NetworkManager.Singleton.StartHost();
+            if (GUILayout.Button("Client")) NetworkManager.Singleton.StartClient();
+            if (GUILayout.Button("Server")) NetworkManager.Singleton.StartServer();
         }
 
         static void StatusLabels() {
-            string mode = NetworkManager.Singleton.IsHost ? "Host" :
-                NetworkManager.Singleton.IsServer ? "Server" : "Client";
+            var mode = NetworkManager.Singleton.IsHost ?
+                "Host" : NetworkManager.Singleton.IsServer ? "Server" : "Client";
 
             GUILayout.Label("Transport: " +
                 NetworkManager.Singleton.NetworkConfig.NetworkTransport.GetType().Name);
-            GUILayout.Label("mode: " + mode);
+            GUILayout.Label("Mode: " + mode);
         }
 
         static void SubmitNewPosition() {
-            if(GUILayout.Button(NetworkManager.Singleton.IsServer ? "Move" : "Request position Change")) {
-                if(NetworkManager.Singleton.ConnectedClients.TryGetValue(NetworkManager.Singleton.LocalClientId, out var networkClient)) {
-                    var player = networkClient.PlayerObject.GetComponent<HelloWorldPlayer>();
-                    if (player)
+            if (GUILayout.Button(NetworkManager.Singleton.IsServer ? "Move" : "Request Position Change")) {
+                if (NetworkManager.Singleton.ConnectedClients.TryGetValue(NetworkManager.Singleton.LocalClientId,
+                    out var networkedClient)) {
+                    var player = networkedClient.PlayerObject.GetComponent<HelloWorldPlayer>();
+                    if (player) {
                         player.Move();
+                    }
                 }
             }
         }
