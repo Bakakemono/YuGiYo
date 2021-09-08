4e67423d4c8f0108e1905ab9fe7e71871dc6f4f1
Bakakemono <robinalves1224@gmail.com>
2021-09-08 18:56:33 +0200

    Add network test and card selection in hand

--- Assets/Scripts/CardDisplyer.cs
+++ Assets/Scripts/CardDisplyer.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using UnityEngine;
+using Quaternion = UnityEngine.Quaternion;
+using Vector3 = UnityEngine.Vector3;
 
 public class CardDisplyer : MonoBehaviour
 {
@@ -14,6 +17,13 @@ public class CardDisplyer : MonoBehaviour
     public Vector3 circlePosition;
 
     List<Vector3> cardPositions = new List<Vector3>();
+
+    const int INVALID_CARD = -1;
+    int currentOverviewedCard = INVALID_CARD;
+    int selectedCard = INVALID_CARD;
+    float overviewDistanceCard = 0.5f;
+    float cardMovingSpeed = 0.2f;
+    Vector3 selectedCardSpotPosition = new Vector3(0, 2.0f);
 
     public float maxAngleRadiant;
     public float fanningMaxAngleRadiant;
@@ -31,6 +41,8 @@ public class CardDisplyer : MonoBehaviour
     }
 
     private void FixedUpdate() {
+        SelectACard();
+
         cardNumber = cards.Count;
         cardPositions.Clear();
         foreach (Transform card in cards)
@@ -44,16 +56,47 @@ public class CardDisplyer : MonoBehaviour
         fanningMaxAngleRadiant = fanningMaxAngle * Mathf.Deg2Rad;
 
         float anglePerCard = fanningMaxAngleRadiant / (cardNumber - 1) < maxAngleRadiant ? fanningMaxAngleRadiant / (cardNumber - 1) : maxAngleRadiant;
-
+         
         for(int i = 0; i < cardNumber; i++) {
-            cardPositions[i] = new Vector3(
-                circlePosition.x + circleRadius * Mathf.Sin((anglePerCard * i) - anglePerCard * (cardNumber - 1.0f) / 2.0f),
-                circlePosition.y + circleRadius * Mathf.Cos((anglePerCard * i) - anglePerCard * (cardNumber - 1.0f) / 2.0f));
+            
 
-            cards[i].localPosition = cardPositions[i];
-            cards[i].localRotation = Quaternion.Euler(new Vector3(0.0f, -10.0f, -((anglePerCard * i) - anglePerCard * (cardNumber - 1.0f) / 2.0f) * Mathf.Rad2Deg));
+            cardPositions[i] = new Vector3(
+                circlePosition.x + (i == currentOverviewedCard ? circleRadius + overviewDistanceCard : circleRadius) * Mathf.Sin((anglePerCard * i) - anglePerCard * (cardNumber - 1.0f) / 2.0f),
+                circlePosition.y + (i == currentOverviewedCard ? circleRadius + overviewDistanceCard : circleRadius) * Mathf.Cos((anglePerCard * i) - anglePerCard * (cardNumber - 1.0f) / 2.0f));
         }
 
+        for (int i = 0; i < cardNumber; i++) {
+            if(i == selectedCard)
+                continue;
+
+            cards[i].localPosition = Vector3.Lerp(cards[i].localPosition, cardPositions[i], cardMovingSpeed);
+            cards[i].localRotation = 
+                Quaternion.Euler(new Vector3(0.0f, -10.0f, -((anglePerCard * i) - anglePerCard * (cardNumber - 1.0f) / 2.0f) * Mathf.Rad2Deg));
+        }
+        if(selectedCard == INVALID_CARD)
+            return;
+        cards[selectedCard].localPosition = Vector3.Lerp(cards[selectedCard].localPosition, fanPosition + selectedCardSpotPosition, cardMovingSpeed);
+        cards[selectedCard].localRotation = Quaternion.Lerp(cards[selectedCard].localRotation, Quaternion.identity, cardMovingSpeed);
+    }
+
+    void SelectACard() {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit)) {
+            Transform cardSelected = hit.transform;
+
+            currentOverviewedCard = cards.FindIndex(x => x == cardSelected);
+        } else {
+            currentOverviewedCard = -1;
+        }
+
+        if (currentOverviewedCard != INVALID_CARD && currentOverviewedCard != selectedCard &&
+            Input.GetMouseButtonUp(0)) {
+            selectedCard = currentOverviewedCard;
+        }
+        else if (currentOverviewedCard == INVALID_CARD && Input.GetMouseButtonUp(0)) {
+            selectedCard = INVALID_CARD;
+        }
     }
 
     //private void OnDrawGizmosSelected() {
--- Assets/Test/NetworkCommandLine.cs
+++ Assets/Test/NetworkCommandLine.cs
@@ -1,49 +1,46 @@
- using System.Collections.Generic;
+using System.Collections.Generic;
 using MLAPI;
 using UnityEngine;
 
-public class NetworkCommandLine : MonoBehaviour
-{
-    private NetworkManager networkManager;
+public class NetworkCommandLine : MonoBehaviour {
+    private NetworkManager netManager;
 
-    private void Start() {
-        networkManager = GetComponentInParent<NetworkManager>();
+    void Start() {
+        netManager = GetComponentInParent<NetworkManager>();
 
-        if (Application.isEditor)
-            return;
+        if (Application.isEditor) return;
 
-        var args = GetCommandLineArgs();
+        var args = GetCommandlineArgs();
 
-        if(args.TryGetValue("-mlapi", out string mlapiValue)) {
+        if (args.TryGetValue("-mlapi", out string mlapiValue)) {
             switch (mlapiValue) {
                 case "server":
-                    networkManager.StartServer();
+                    netManager.StartServer();
                     break;
                 case "host":
-                    networkManager.StartHost();
+                    netManager.StartHost();
                     break;
                 case "client":
-                    networkManager.StartClient();
+                    netManager.StartClient();
                     break;
             }
         }
     }
 
-    private Dictionary<string, string> GetCommandLineArgs() {
+    private Dictionary<string, string> GetCommandlineArgs() {
         Dictionary<string, string> argDictionary = new Dictionary<string, string>();
 
         var args = System.Environment.GetCommandLineArgs();
 
-        for(int i = 0; i <args.Length; i++) {
+        for (int i = 0; i < args.Length; ++i) {
             var arg = args[i].ToLower();
             if (arg.StartsWith("-")) {
                 var value = i < args.Length - 1 ? args[i + 1].ToLower() : null;
                 value = (value?.StartsWith("-") ?? false) ? null : value;
-                
+
                 argDictionary.Add(arg, value);
             }
         }
         return argDictionary;
     }
-
 }
