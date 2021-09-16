c22d6a3a15686b4ce396926697edcc056426794a
Bakakemono <robinalves1224@gmail.com>
2021-09-16 17:50:20 +0200

    Fixe Draw from draw pile

--- Assets/Scripts/CardDisplyer.cs
+++ Assets/Scripts/CardDisplyer.cs
@@ -28,6 +28,8 @@ public class CardDisplyer : MonoBehaviour
     public float maxAngleRadiant;
     public float fanningMaxAngleRadiant;
 
+    [SerializeField] bool isPlayerTurn = false;
+
     void Start() {
         cardNumber = cards.Count;
         foreach (Transform card in cards) {
@@ -41,6 +43,9 @@ public class CardDisplyer : MonoBehaviour
     }
 
     private void FixedUpdate() {
+        if(Input.GetMouseButtonUp(0) && isPlayerTurn)
+            DrawCard();
+
         SelectACard();
 
         cardNumber = cards.Count;
@@ -55,14 +60,17 @@ public class CardDisplyer : MonoBehaviour
         maxAngleRadiant = maxAngle * Mathf.Deg2Rad;
         fanningMaxAngleRadiant = fanningMaxAngle * Mathf.Deg2Rad;
 
-        float anglePerCard = fanningMaxAngleRadiant / (cardNumber - 1) < maxAngleRadiant ? fanningMaxAngleRadiant / (cardNumber - 1) : maxAngleRadiant;
+        float anglePerCard = fanningMaxAngleRadiant / (cardNumber - 1) < maxAngleRadiant ?
+                                    fanningMaxAngleRadiant / (cardNumber - 1) : maxAngleRadiant;
          
         for(int i = 0; i < cardNumber; i++) {
-            
-
             cardPositions[i] = new Vector3(
-                circlePosition.x + (i == currentOverviewedCard ? circleRadius + overviewDistanceCard : circleRadius) * Mathf.Sin((anglePerCard * i) - anglePerCard * (cardNumber - 1.0f) / 2.0f),
-                circlePosition.y + (i == currentOverviewedCard ? circleRadius + overviewDistanceCard : circleRadius) * Mathf.Cos((anglePerCard * i) - anglePerCard * (cardNumber - 1.0f) / 2.0f));
+                circlePosition.x + 
+                    (i == currentOverviewedCard ? circleRadius + overviewDistanceCard : circleRadius) *
+                        Mathf.Sin((anglePerCard * i) - anglePerCard * (cardNumber - 1.0f) / 2.0f),
+                circlePosition.y + 
+                    (i == currentOverviewedCard ? circleRadius + overviewDistanceCard : circleRadius) *
+                        Mathf.Cos((anglePerCard * i) - anglePerCard * (cardNumber - 1.0f) / 2.0f));
         }
 
         for (int i = 0; i < cardNumber; i++) {
@@ -73,6 +81,7 @@ public class CardDisplyer : MonoBehaviour
             cards[i].localRotation = 
                 Quaternion.Euler(new Vector3(0.0f, -10.0f, -((anglePerCard * i) - anglePerCard * (cardNumber - 1.0f) / 2.0f) * Mathf.Rad2Deg));
         }
+
         if(selectedCard == INVALID_CARD)
             return;
         cards[selectedCard].localPosition = Vector3.Lerp(cards[selectedCard].localPosition, fanPosition + selectedCardSpotPosition, cardMovingSpeed);
@@ -86,7 +95,8 @@ public class CardDisplyer : MonoBehaviour
             Transform cardSelected = hit.transform;
 
             currentOverviewedCard = cards.FindIndex(x => x == cardSelected);
-        } else {
+        }
+        else {
             currentOverviewedCard = -1;
         }
 
@@ -99,27 +109,40 @@ public class CardDisplyer : MonoBehaviour
         }
     }
 
-    //private void OnDrawGizmosSelected() {
-    //    Gizmos.color = Color.red;
+    void DrawCard() {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit)) {
+            DrawPileManager drawPileManager = hit.transform.GetComponent<DrawPileManager>();
+            if (drawPileManager) {
+                Transform newCard = drawPileManager.DrawCard();
+                newCard.parent = transform;
+                cards.Add(newCard);
+            }
+        }
+    }
 
-    //    maxAngleRadiant = maxAngle / 360.0f * 2 * Mathf.PI;
-    //    circlePosition = fanPosition - new Vector3(0.0f, circleRadius, 0.0f);
+    private void OnDrawGizmosSelected() {
+        Gizmos.color = Color.red;
+
+        maxAngleRadiant = maxAngle / 360.0f * 2 * Mathf.PI;
+        circlePosition = fanPosition - new Vector3(0.0f, circleRadius, 0.0f);
 
 
-    //    Gizmos.DrawSphere(circlePosition, 0.3f);
+        Gizmos.DrawSphere(transform.position + circlePosition, 0.3f);
 
-    //    Gizmos.DrawLine(
-    //        circlePosition,
-    //            new Vector3(
-    //                circlePosition.x + circleRadius * Mathf.Sin(-fanningMaxAngle * Mathf.Deg2Rad / 2.0f),
-    //                circlePosition.y + circleRadius * Mathf.Cos(-fanningMaxAngle * Mathf.Deg2Rad / 2.0f))
-    //        );
+        Gizmos.DrawLine(
+            transform.position + circlePosition,
+                transform.position + new Vector3(
+                    circlePosition.x + circleRadius * Mathf.Sin(-fanningMaxAngle * Mathf.Deg2Rad / 2.0f),
+                    circlePosition.y + circleRadius * Mathf.Cos(-fanningMaxAngle * Mathf.Deg2Rad / 2.0f))
+            );
 
-    //    Gizmos.DrawLine(
-    //        circlePosition,
-    //            new Vector3(
-    //                circlePosition.x + circleRadius * Mathf.Sin(fanningMaxAngle * Mathf.Deg2Rad / 2.0f),
-    //                circlePosition.y + circleRadius * Mathf.Cos(fanningMaxAngle * Mathf.Deg2Rad / 2.0f))
-    //       );
-    //}
+        Gizmos.DrawLine(
+            transform.position + circlePosition,
+                transform.position + new Vector3(
+                    circlePosition.x + circleRadius * Mathf.Sin(fanningMaxAngle * Mathf.Deg2Rad / 2.0f),
+                    circlePosition.y + circleRadius * Mathf.Cos(fanningMaxAngle * Mathf.Deg2Rad / 2.0f))
+           );
+    }
 }
---
+++ Assets/Scripts/DrawPileManager.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawPileManager : MonoBehaviour
+{
+    List<Transform> cards = new List<Transform>();
+    float cardThickness = 0.01f;
+    float spacebetweenCard = 0.0f;
+    Vector2 cardDimension = new Vector2(1.2f, 2.2f);
+
+    BoxCollider boxCollider;
+
+    bool playerTurn = true;
+
+    void Start() {
+        Card[] currentCards = GetComponentsInChildren<Card>();
+        foreach (Card card in currentCards) {
+            cards.Add(card.GetComponent<Transform>());
+        }
+
+        for (int i = 0; i < cards.Count; i++) {
+            cards[i].localPosition = new Vector3(0, 0, cardThickness / 2.0f + (cards.Count - i) * (cardThickness + spacebetweenCard));
+        }
+
+        boxCollider = GetComponent<BoxCollider>();
+        CalculateHitBoxSize();
+    }
+
+    void CalculateHitBoxSize() {
+        boxCollider.size = new Vector3(cardDimension.x, cardDimension.y, (cardThickness + spacebetweenCard) * cards.Count);
+        boxCollider.center = new Vector3(0, 0, (cardThickness + spacebetweenCard) * cards.Count / 2.0f);
+    }
+
+    public Transform DrawCard() {
+        Transform DrawedCard = cards[0];
+        cards.RemoveAt(0);
+        CalculateHitBoxSize();
+        return DrawedCard;
+    }
+}
